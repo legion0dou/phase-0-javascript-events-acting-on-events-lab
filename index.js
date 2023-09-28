2c67740e7ee5cfcca31e5b625551f332384b3812
user_name <email_id>
2023-09-28 09:09:30 +0300

    complete assignment

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 // Your code here
+const dodger = document.getElementById("dodger");
+function moveDodgerLeft() {
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10);
+  
+    if (left > 0) {
+      dodger.style.left = `${left - 1}px`;
+    }
+  }
+  document.addEventListener("keydown", function (e) {
+    if (e.key === "ArrowLeft") {
+      moveDodgerLeft();
+    }
+  });
+  function moveDodgerRight() {
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10);
+  
+    // Assuming the game field width is 400 pixels
+    const gameFieldWidth = 400;
+  
+    if (left + dodger.clientWidth < gameFieldWidth) {
+      dodger.style.left = `${left + 1}px`;
+    }
+  }
+  
+  // Attach the moveDodgerRight function to a key event (e.g., right arrow key)
+  document.addEventListener("keydown", function (event) {
+    if (event.key === "ArrowRight") {
+      moveDodgerRight();
+    }
+  });
+  
